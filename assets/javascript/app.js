8a65165832d361c9696c3fb5995bbb58dffd284f
Zeus <reeseborchersmsu@gmail.com>
2019-07-06 12:57:08 -0500

    3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $("button").on("click", function () {
-    var mySearch = $('#form-group').val;
+    var mySearch = $('#form-group').val();
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + mySearch + "&api-key=7DUQIH9nNCwbzAF682173JSVlbKHpBwz";
 
 

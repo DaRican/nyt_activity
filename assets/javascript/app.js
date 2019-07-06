803c6cc599c3aee33d037d51b6afaa56b6581dd8
Zeus <reeseborchersmsu@gmail.com>
2019-07-06 12:56:02 -0500

    3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,25 @@
-<<<<<<< HEAD
 $("button").on("click", function () {
-    var mySearch = $(this).attr("*search-term*");
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=7DUQIH9nNCwbzAF682173JSVlbKHpBwz"
+    var mySearch = $('#form-group').val;
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + mySearch + "&api-key=7DUQIH9nNCwbzAF682173JSVlbKHpBwz";
+
+
+
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+
+        var results = response.data;
+        console.log(reponse);
+
+
+
+        //for (var i = 0;)
+    }
+
+
+    )
 }
-=======
 
-queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey";
-
-$.ajax({
-    url: queryUrl
-})
->>>>>>> 01e58c56e7a7d858bd382071a77db1a3dd65157a
+);

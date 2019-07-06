2adc7d49f6bfe18442cc74733683daa2594dc6f2
Zeus <reeseborchersmsu@gmail.com>
2019-07-06 12:32:24 -0500

    1

---
+++ assets/javascript/app.js
@@ -0,0 +1,4 @@
+$("button").on("click", function () {
+    var mySearch = $(this).attr("*search-term*");
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=7DUQIH9nNCwbzAF682173JSVlbKHpBwz"
+}
